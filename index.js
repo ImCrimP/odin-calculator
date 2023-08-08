bae81a4dc38bc26ef4c2b1efa06bb9ff5a30a7df
ImCrimP <pjmountin@me.com>
2023-08-08 03:31:28 -0500

    display input when pressed

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
-const display = document.querySelector("#asn");
-const prevCalc = document.querySelector("#prevCalc");
+//const display = document.querySelector("#asn");
+//const prevCalc = document.querySelector("#prevCalc");
+const calc = document.querySelector("#screen");
+
+const prevCalc = document.createElement("div");
+prevCalc.setAttribute("id", "prevCalc");
+prevCalc.textContent = "";
+calc.appendChild(prevCalc);
+
+const display = document.createElement("div");
+display.setAttribute("id","ans");
+display.textContent = "0";
+calc.appendChild(display);
+
+
+
 
 const buttons = document.querySelectorAll(".btn");
 
@@ -20,6 +34,8 @@ let decClick = false;
 
 const equal = document.querySelector("#eq");
 let eqClick = false;
+
+let answer;
 
 const numArray = [
     document.querySelector("#zero"),
@@ -49,7 +65,6 @@ run();
 
 
 function run(){
-    
     numClick();
     operationClick();
     clearClick();
@@ -65,22 +80,56 @@ function numClick(){
         numArray[i].addEventListener("click", ()=>{
             if(!opClick){
                 firstVal += numArray[i].textContent;
-                console.log("first value " + firstVal)
+                firstValInt = parseInt(firstVal);
+                console.log("first value " + firstValInt);
+                prevCalc.textContent =  `${secondVal} ${operator}`;
+                display.textContent = firstVal;
             }
             else{
                 secondVal += numArray[i].textContent;
-                console.log("second value " + secondVal);
+                secondValInt = parseInt(secondVal);
+                console.log("second value " + secondValInt);
+                prevCalc.textContent = `${firstVal} ${operator}`;
+                display.textContent = secondVal;
             }
         });
     }
 }
 
 function operationClick(){
+
+    /*
+    let div = operator[0];
+    let mul = operator[1];
+    let sub = operator[2];
+    let add = operator[3];
+    */
+
     for(let i = 0; i < operatorsArray.length; i++){
         operatorsArray[i].addEventListener("click", () =>{
             if(firstVal != ''){
                 operator = operatorsArray[i].textContent;
-                opClick = true;
+                if(!opClick){
+                    //prevCalc.textContent += operator;
+
+                    /*if(secondVal != ""){
+                        if(operator == "÷"){
+                            answer = secondVal / firstVal;
+                            display.textContent = answer;
+                            prevCalc = `${secondVal} ÷ ${firstVal}`;
+                        }
+                    }
+                    */
+                    firstVal.textContent = "";
+                    prevCalc.textContent = `${firstVal} ${operator}`;
+                    opClick = true;
+                }
+                else{
+                    secondVal.textContent = ""
+                    prevCalc.textContent = `${secondVal} ${operator}`;
+                    opClick = false;
+                }
+                
                 console.log("operation " + operator); 
             }
             decClick = false;    
@@ -95,22 +144,40 @@ function clearClick(){
         clearClicked = true;
         opClick = false;
         decClick = false;
+        operator = "";
+        prevCalc.textContent = "";
+        display.textContent = "0";
     })
 }
 
 function deleteClick(){
     deleteBtn.addEventListener("click", () => {
+        
         if(!opClick){
-            let lastChar = firstVal.charAt(firstVal.length - 1);
-            lastChar == "." ? decClick = false : decClick = true;
-            firstVal = firstVal.slice(0, -1);
-            console.log("first value " + firstVal);
+        
+            if(firstVal != ""){
+                let lastChar = firstVal.charAt(firstVal.length - 1);
+                lastChar == "." ? decClick = false : decClick = true;
+                firstVal = firstVal.slice(0, -1);
+                console.log("first value " + firstVal);
+                display.textContent = firstVal;
+                if(firstVal == ""){
+                    display.textContent = "0"
+                }
+            }
         }
         else{
-            let lastChar = secondVal.charAt(secondVal.length - 1);
-            lastChar == "." ? decClick = false : decClick = true;
-            secondVal = secondVal.slice(0, -1);
-            console.log("second Value " + secondVal);
+            
+            if(secondVal.length > 0){
+                let lastChar = secondVal.charAt(secondVal.length - 1);
+                lastChar == "." ? decClick = false : decClick = true;
+                secondVal = secondVal.slice(0, -1);
+                console.log("second Value " + secondVal);
+                display.textContent = secondVal;
+                if(secondVal == ""){
+                    display.textContent = "0";
+                }
+            } 
         }
     })
 }
@@ -120,10 +187,12 @@ function decimalClick(){
         if(!decClick && !opClick){
             decClick = true;
             firstVal += "."
+            display.textContent = firstVal;
         }
         else if(!decClick && opClick){
             decClick = true;
             secondVal += "."
+            display.textContent = secondVal;
         }
     })
 }
@@ -138,9 +207,17 @@ function equalClick(){
 }
 
 function displayInput(){
+    //display
+
+    /*
     for(let i = 0; i < buttons.length; i++){
         buttons.addEventListener("click", () => {
 
         })
     }
+    */
+}
+
+function displayOpOrEq (){
+
 }
